70ca237e256791f9aee3d54cb95d3e70709c598b
Javier E. Zapanta <me@javierzapanta.com>
2023-05-21 02:45:29 -0400

    Added PizzaController

---
+++ ContossoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,33 @@
+using ContossoPizza.Models;
+using ContossoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContossoPizza.Controllers;
+
+/// <summary>
+/// JEZ:  Controller to handle request
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public PizzaController()
+    {
+
+    }
+
+
+    // GET all action
+
+    // GET by Id action
+
+    // POST action
+
+    // PUT action
+
+    // DELETE action
+
+}
