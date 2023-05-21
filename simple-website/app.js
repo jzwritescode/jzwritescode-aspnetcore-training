53772dff5b3501dbb4cf17550b9de004e200a089
Javier E. Zapanta <me@javierzapanta.com>
2023-05-20 23:14:28 -0400

    Added code to switch button text in app.js

--- simple-website/app.js
+++ simple-website/app.js
@@ -4,7 +4,15 @@
 const switcher = document.querySelector('.btn');
 
 // add event handler for "click" event
-switcher.addEventListener('click', function() {
+switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
+
+    // add code to switch button text
+    const className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
 });
