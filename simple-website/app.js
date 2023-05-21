306f933c58740e1dfe64ff92d6668fac16bc078b
Javier E. Zapanta <me@javierzapanta.com>
2023-05-20 23:13:50 -0400

    Added event handler for button in app.js

--- simple-website/app.js
+++ simple-website/app.js
@@ -1,2 +1,10 @@
 'use strict';
 
+// build reference to button
+const switcher = document.querySelector('.btn');
+
+// add event handler for "click" event
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+});
